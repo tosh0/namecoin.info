0d0132f500e1413e1dea34343f7ddd5eab44dee3
indolering <zachlym@indolering.com>
2014-12-08 18:19:28 -0800

    Added team page
    Added Team page from GDoc.  All bios were signed off by their
    respective people.

---
+++ team.php
@@ -0,0 +1,37 @@
+<div class="row">
+<p class="lead">Namecoin is developed by an international team of academics and volunteers working to protect free speech online and save the world from an Orwellian surveillance state. </p>
+    <h2>Daniel Kraft (domob)</h2>
+    <strong>Chief Namecoin Scientist</strong>
+    <p>Daniel is our lead C++ developer, currently working on namecoind, Namecoin-Qt, and libcoin. Daniel is also our specialist for identity applications, being the author of NameID, the Bitmessage Namecoin integration, and the OTR Namecoin integration. Daniel is currently a PhD student at University of Graz in Austria.</p>
+    <h2>Phelix</h2>
+    <strong>Lead Windows Developer &amp; Community Organizer</strong>
+    <p>Phelix (who goes by a pseudonym) is our lead Windows developer, and works on namecoind, Namecoin-Qt, NMControl, and other projects such as ANTPY. Phelix also handles financial matters and (along with Jeremy and Indolering) fundraising, and is our longest-serving team member.</p>
+    <h2>Ryan Castellucci</h2>
+    <strong>Lead Security Engineer</strong>
+    <p>Ryan is our security specialist and all-around-badass, handling everything from web infrastructure to C++ development to crisis management.</p>
+    <h2>Jeremy Rand (biolizard89)</h2>
+    <strong>Lead Application Engineer &amp; Community Organizer</strong>
+    <p>Jeremy is our .bit application specialist, and works on FreeSpeechMe, NMControl, and other projects such as DyName. Jeremy also handles fundraising (along with Indolering and Phelix), and acts as our build engineer. Jeremy is currently a computer science undergraduate at University of Oklahoma in the U.S.</p>
+    <h2>Zachary Lym (indolering)</h2>
+    <strong>Lead UX Engineer &amp; Community Organizer</strong>
+    <p>Indolering (who prefers his pseudonym) is our UX engineer and lead developer for Speech.is as well as acting as a community organizer and (along with Jeremy and Phelix) fundraiser. He is currently an informatics &amp; psychology undergraduate at University of Washington in the U.S., and has interned at Mozilla.</p>
+    <h2>Andrew Colosimo (Snailbrain)</h2>
+    <strong>Developer &amp; Community Organizer</strong>
+    <p>Snailbrain (who prefers his pseudonym) is an IT specialist who initiated the creation of Namecoin-Qt alongside Mikhail, and was the main funder of Namecoin development during most of 2013; he also contributes to testing and development. Snailbrain is also the creator of the Huntercoin concept, which is currently Namecoin&rsquo;s only fork. Huntercoin&rsquo;s current development has improved Namecoin on many levels with the help of the coding expertise of Daniel.</p>
+    <h2>Cassini</h2>
+    <strong>OS X Maintainer</strong>
+    <p>Cassini (working under a pseudonym) holds the reins in the Mac OS X world. He is another dauntless tester and one of our blockchain watchdogs.</p>
+    <h2>Peter Conrad (pmc)</h2>
+    <strong>Linux Build Maintainer</strong>
+    <p>Peter uses his 20 years of Unix/Linux experience to manage namecoin packages for the major linux distributions. Outside the Namecoin community he is working as a freelance software developer.</p>
+    <h2>Anonymous Developers</h2>
+    <p>We have several developers who prefer to remain anonymous; their anonymity does not reflect the incredible impact their contributions have made to Namecoin. We are deeply grateful for their work.</p>
+    <h1>Former Developers</h1>
+    <p>We have several developers who are no longer active in the community, listed below.</p>
+    <h2>Mikhail Sindeyev (thecoder)</h2>
+    <p>Mikhail (then acting under a pseudonym) was our lead C++ developer for roughly a year (prior to Daniel), working on namecoind and creating Namecoin-Qt. Mikhail was a PhD student at Moscow State University. In 2014 Mikhail tragically died of a stroke at the age of 29. His identity was made public shortly after his death so that his great contribution to free speech technology would be recognized.</p>
+    <h2>Khalahan Henkh (khal)</h2>
+    <p>Khalahan (a pseudonym) was our lead C++ developer for roughly two years (prior to Mikhail), working on namecoind and creating NMControl. Khalahan is currently retired.</p>
+    <h2>Vincent Durham (vinced)</h2>
+    <p>Vincent (a pseudonym) was the creator of Namecoin, forking Bitcoin&rsquo;s code to create the first solution to Zooko&rsquo;s Triangle. Vincent is currently retired.</p>
+</div>
