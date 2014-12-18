8a29a27b2c85a7f1b5ab8b7719f8033ab3274f04
phelixbtc <github@blockchained.com>
2014-12-18 11:19:03 +0100

    Block reward halving

--- home.php
+++ home.php
@@ -30,6 +30,7 @@
 
 <div class="row">
     <h2>News</h2>
+    <p><b>2014-12-16</b> As of block <b>210000</b> the Namecoin block reward halved to <b>25NMC</b>. Happy halving day!</p>
     <p><b>2014-09-12</b> Out now: <b>Namecoin v0.3.76</b> - <a target="_blank" href="http://forum.namecoin.info/viewtopic.php?f=8&t=1975">Release info</a> -
         <a href="?p=download">Download</a></p>
     <p><b>2014-04-20</b> Thanks to <b>Shobute</b> for designing and <b>Indolering</b> for pushing the <b>new website</b>.</p>
