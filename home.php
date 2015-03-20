485dbe848d3fe913d7c972d5a8c87eef5249f2dc
phelixbtc <github@blockchained.com>
2015-03-20 23:11:28 +0100

    News: fix

--- home.php
+++ home.php
@@ -31,7 +31,7 @@
 <div class="row">
     <h2>News</h2>
     <p><b>2015-03-20</b> <a href="http://n-o-d-e.net/post/113777384551/the-namecoin-interview-censorship-resistant" target="_blank">Interview</a>
-        Interview with Namecoin lead developer Daniel Kraft.</p>
+        with Namecoin lead developer Daniel Kraft.</p>
     <p><b>2014-12-19</b> <b>Softfork</b> upcoming with block 212500 (around New Year's Eve). Pool operators and solo miners please
         <a href="?p=download">update to v0.3.80</a> asap. Regular users are recommended to update, too.
         See the <a href="https://forum.namecoin.info/viewtopic.php?p=13717">Release notes.</a></p>
