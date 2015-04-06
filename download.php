99375c4a72e6a75708826d1266c2eba61304b93b
JeremyRand <biolizard89@gmail.com>
2015-04-06 05:38:06 -0500

    Cleanup messy HTML in download page.  (It was literally unreadable to me before this cleanup.)

--- download.php
+++ download.php
@@ -1,22 +1,27 @@
 <div class="row">
-<div class="col-sm-4">
-<h3>Source</h3>
-Fork us at <a href="https://github.com/namecoin/namecoin">Github</a>. </div>
-<div class="col-sm-4">
-<h3>Windows</h3>
-Download <a href="https://namecoin.info/files/Namecoin_v0.3.80_setup.exe">v0.3.80 installer</a>.<br>
-<a target="_blank" href="https://forum.namecoin.info/viewtopic.php?f=8&t=2123">Release information and signatures</a>.</div>
-<div class="col-sm-4">
-<h3>Mac OS X</h3>
-Download <a href="https://namecoin.info/files/Namecoin-Qt.app-0.3.80-a00c33d.zip">v0.3.80 Namecoin-Qt</a>.<br>
-<a target="_blank" href="https://forum.namecoin.info/viewtopic.php?f=8&t=2235">Release information</a>.</div>
-</div>
+	<div class="col-sm-4">
+		<h3>Source</h3>
+		<p>Fork us at <a href="https://github.com/namecoin/namecoin">Github</a>.</p>
+	</div>
+	<div class="col-sm-4">
+		<h3>Windows</h3>
+		<p>
+			Download <a href="https://namecoin.info/files/Namecoin_v0.3.80_setup.exe">v0.3.80 installer</a>.<br>
+			<a target="_blank" href="https://forum.namecoin.info/viewtopic.php?f=8&t=2123">Release information and signatures</a>.
+		</p>
+	</div>
+	<div class="col-sm-4">
+		<h3>Mac OS X</h3>
+		<p>
+			Download <a href="https://namecoin.info/files/Namecoin-Qt.app-0.3.80-a00c33d.zip">v0.3.80 Namecoin-Qt</a>.<br>
+			<a target="_blank" href="https://forum.namecoin.info/viewtopic.php?f=8&t=2235">Release information</a>.
+		</p>
+	</div>
 </div>
 <br><br>
 <div class="row">
-<div class="col-md-12">
-<h3>Linux Packages</h3>
-<p><a href="https://software.opensuse.org/download.html?project=home%3Ap_conrad%3Acoins&amp;package=namecoin">Linux packages provided by OBS</a>.</p>
+	<div class="col-md-12">
+		<h3>Linux Packages</h3>
+		<p><a href="https://software.opensuse.org/download.html?project=home%3Ap_conrad%3Acoins&amp;package=namecoin">Linux packages provided by OBS</a>.</p>
         </div>
-    </div>
-    </div>
+</div>
