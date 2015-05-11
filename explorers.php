a0df44071dda0023027f19ce7859cf6b2601a876
phelixbtc <github@blockchained.com>
2015-05-11 11:12:10 +0200

    explorers page: fix link, formatting

--- explorers.php
+++ explorers.php
@@ -1,20 +1,18 @@
 <div class="row">
+    <span style="font-size:130%;">
     <p><b>Namecoin Blockchain Explorers</b></p>
-    <span style="font-size:100%;">
-        <p><b>with Support for Name Operations</b></p>
-        <a target="_blank" href="https://namecha.in/">namecha.in</a><br>
-        <br>
     </span>
-    <span style="font-size:100%;">
-        <p><b>with Basic Blockchain Support </b></p>
-        <a target="_blank" href="https://bkchain.org/nmc">bkchain</a><br>
-        <a target="_blank" href="https://bchain.info/NMC/">Bchain.info</a><br>
-        <a target="_blank" href="https://bitinfocharts.com/de/namecoin/explorer/">BitInfoCharts</a><br>
-        <a target="_blank" href="https://coinplorer.com/NMC">Coinplorer</a><br>
-        <br>
-    </span>
+    <p>with Support for Name Operations</p>
+    <a target="_blank" href="https://namecha.in/">namecha.in</a><br>
+    <br>
+    <p>with Basic Blockchain Support</p>
+    <a target="_blank" href="https://bkchain.org/nmc">bkchain</a><br>
+    <a target="_blank" href="https://bchain.info/NMC/">Bchain.info</a><br>
+    <a target="_blank" href="https://bitinfocharts.com/de/namecoin/explorer/">BitInfoCharts</a><br>
+    <a target="_blank" href="https://coinplorer.com/NMC">Coinplorer</a><br>
+    <br>
 
-    <p>To get on the list please post on the <a target="_blank" href="https://forum.namecoin.org>forum</a>.</p>
+    <p>To get on the list please post on the <a target="_blank" href="https://forum.namecoin.org">forum</a>.</p>
 
 </div>
 
