3e2791e6256cd07c298efe374d25751c55a93ac9
phelixbtc <github@blockchained.com>
2015-10-11 22:20:19 +0200

    download: grammar, wording

--- download.php
+++ download.php
@@ -30,7 +30,7 @@
 <div class="row">
     <h2>NMControl Middleware</h2>
         <p>
-            NMControl connects your browser/application to the client (or soon to an API). It allows for example to browse .bit domains.
+            NMControl connects your browser/application to the client (or soon to an API). It allows you, for example, to browse .bit domains.
             <a href="https://github.com/namecoin/nmcontrol">Source on Github</a>.
         </p>
         <div class="col-sm-4">
@@ -46,8 +46,7 @@
             <p>
                 With Python 2.7 installed you can run nmcontrol.py directly from the <a href="https://github.com/namecoin/nmcontrol">source</a>.
                 Make sure you have a Namecoin client running with the blockchain fully downloaded.
-                You can find additional information <a target="_blank" href="https://forum.namecoin.info/viewtopic.php?f=8&t=2402">
-                here</a>.
+                If you need help ask on Github or on the <a target="_blank" href="https://forum.namecoin.info/viewtopic.php?f=8&t=2402">forum</a>.
             </p>
         </div>
 
